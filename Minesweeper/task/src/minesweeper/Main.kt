45c1fd957115c4aa6170b6721362bd4588e8c65f
Esteban <cepalacios@outlook.com>
2020-09-26 13:07:22 -0500

    almost finished, the game runs, it sets the * or . according to user input, and if all mines are marked is a win

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -2,6 +2,7 @@ package minesweeper
 
 import java.io.InputStream
 import java.util.*
+import kotlin.collections.HashSet
 import kotlin.random.Random
 
 // constant size for the grid
@@ -29,16 +30,32 @@ class MineArray() {
     }
 }
 
+
+//enum class Cell(val type: String, var cellNum: Int, val isBomb: Boolean) {
+//    NUMBER_CELL("0", 0, false),
+//    BOMB_CELL("X", 0, false),
+//    SAFE_CELL(".", 0, true);
+//}
+
 class Cell() {
+    var content = "."
     var cellNum = 0
     var isBomb = false
 
 }
 
 class Grid() {
+    //receives user input and calculate the cellNum
+
+    fun getCellNum(row: Int, column: Int) = column * size + row
+
+    //fun where first adds the player choice and adds it to a mutable list, then it uses a while runs until the mutableList and the mineArray are equal
+
     //    creates the game's grid with the size of the board squared and sets values default to "."
-    val fullGrid = Array(size * size) { "." }
-    fun makeGrid(mineArray: IntArray): Array<String> {
+    val fullGrid: Array<Cell> = Array(size * size) { Cell() }
+
+
+    fun makeGrid(mineArray: IntArray): Array<Cell> {
 //    makes and stores the grid into gridArray
 
         for (i in 0 until size) {
@@ -49,103 +66,104 @@ class Grid() {
                 cell.cellNum = i * size + j
 //                checks if the cellNum is in the mine array to change the cell from the default "." to "X"
                 if (cell.cellNum in mineArray) {
-                    fullGrid[cell.cellNum] = "X"
+                    cell.isBomb = true
+                    fullGrid[cell.cellNum] = cell
                 }
-//                in the loop goes by every mine "X" and modifies the cells around it
-                if (fullGrid[cell.cellNum] == "X") {
+//                in the loop goes by every mine "isBomb" and modifies the cells around it
+                if (cell.isBomb) {
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is 0 the cell modification doesn't take place because the bomb won't modify the cell 1 row up
                     if (fullGrid.getOrNull(cell.cellNum - 1) != null && j != 0) {
 //                        checks that the cell to be modified isn't a mine
-                        if (fullGrid[cell.cellNum - 1] != "X") {
+                        if (!fullGrid[cell.cellNum - 1].isBomb) {
 //                            checks that if the cell to be modified is "." changes to 1, otherwise changes to Int and then increases by 1 then returns to String
-                            if (fullGrid[cell.cellNum - 1] == ".") {
-                                fullGrid[cell.cellNum - 1] = "1"
+                            if (fullGrid[cell.cellNum - 1].content == ".") {
+                                fullGrid[cell.cellNum - 1].content = "1"
                             } else {
-                                fullGrid[cell.cellNum - 1] =
-                                        (fullGrid[cell.cellNum - 1].toInt() + 1).toString()
+                                fullGrid[cell.cellNum - 1].content =
+                                        (fullGrid[cell.cellNum - 1].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is 0 the cell modification doesn't take place because the bomb won't modify the cell two rows up
                     if (fullGrid.getOrNull(cell.cellNum - 10) != null && j != 0) {
-                        if (fullGrid[cell.cellNum - 10] != "X") {
-                            if (fullGrid[cell.cellNum - 10] == ".") {
-                                fullGrid[cell.cellNum - 10] = "1"
+                        if (!fullGrid[cell.cellNum - 10].isBomb) {
+                            if (fullGrid[cell.cellNum - 10].content == ".") {
+                                fullGrid[cell.cellNum - 10].content = "1"
                             } else {
-                                fullGrid[cell.cellNum - 10] =
-                                        (fullGrid[cell.cellNum - 10].toInt() + 1).toString()
+                                fullGrid[cell.cellNum - 10].content =
+                                        (fullGrid[cell.cellNum - 10].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
                     if (fullGrid.getOrNull(cell.cellNum - 9) != null) {
-                        if (fullGrid[cell.cellNum - 9] != "X") {
-                            if (fullGrid[cell.cellNum - 9] == ".") {
-                                fullGrid[cell.cellNum - 9] = "1"
+                        if (!fullGrid[cell.cellNum - 9].isBomb) {
+                            if (fullGrid[cell.cellNum - 9].content == ".") {
+                                fullGrid[cell.cellNum - 9].content = "1"
                             } else {
-                                fullGrid[cell.cellNum - 9] =
-                                        (fullGrid[cell.cellNum - 9].toInt() + 1).toString()
+                                fullGrid[cell.cellNum - 9].content =
+                                        (fullGrid[cell.cellNum - 9].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is size - 1 (8 in this case) the cell modification doesn't take place because the bomb won't modify the first cell of the same row
                     if (fullGrid.getOrNull(cell.cellNum - 8) != null && j != size - 1) {
-                        if (fullGrid[cell.cellNum - 8] != "X") {
-                            if (fullGrid[cell.cellNum - 8] == ".") {
-                                fullGrid[cell.cellNum - 8] = "1"
+                        if (!fullGrid[cell.cellNum - 8].isBomb) {
+                            if (fullGrid[cell.cellNum - 8].content == ".") {
+                                fullGrid[cell.cellNum - 8].content = "1"
                             } else {
-                                fullGrid[cell.cellNum - 8] =
-                                        (fullGrid[cell.cellNum - 8].toInt() + 1).toString()
+                                fullGrid[cell.cellNum - 8].content =
+                                        (fullGrid[cell.cellNum - 8].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is size - 1 (8) the cell modification doesn't take place because the bomb won't modify the cell one row below
                     if (fullGrid.getOrNull(cell.cellNum + 1) != null && j != size - 1) {
-                        if (fullGrid[cell.cellNum + 1] != "X") {
-                            if (fullGrid[cell.cellNum + 1] == ".") {
-                                fullGrid[cell.cellNum + 1] = "1"
+                        if (!fullGrid[cell.cellNum + 1].isBomb) {
+                            if (fullGrid[cell.cellNum + 1].content == ".") {
+                                fullGrid[cell.cellNum + 1].content = "1"
                             } else {
-                                fullGrid[cell.cellNum + 1] =
-                                        (fullGrid[cell.cellNum + 1].toInt() + 1).toString()
+                                fullGrid[cell.cellNum + 1].content =
+                                        (fullGrid[cell.cellNum + 1].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is 0 the cell modification doesn't take place because the bomb won't modify the last cell of the same row
                     if (fullGrid.getOrNull(cell.cellNum + 8) != null && j != 0) {
-                        if (fullGrid[cell.cellNum + 8] != "X") {
-                            if (fullGrid[cell.cellNum + 8] == ".") {
-                                fullGrid[cell.cellNum + 8] = "1"
+                        if (!fullGrid[cell.cellNum + 8].isBomb) {
+                            if (fullGrid[cell.cellNum + 8].content == ".") {
+                                fullGrid[cell.cellNum + 8].content = "1"
                             } else {
-                                fullGrid[cell.cellNum + 8] =
-                                        (fullGrid[cell.cellNum + 8].toInt() + 1).toString()
+                                fullGrid[cell.cellNum + 8].content =
+                                        (fullGrid[cell.cellNum + 8].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
                     if (fullGrid.getOrNull(cell.cellNum + 9) != null) {
-                        if (fullGrid[cell.cellNum + 9] != "X") {
-                            if (fullGrid[cell.cellNum + 9] == ".") {
-                                fullGrid[cell.cellNum + 9] = "1"
+                        if (!fullGrid[cell.cellNum + 9].isBomb) {
+                            if (fullGrid[cell.cellNum + 9].content == ".") {
+                                fullGrid[cell.cellNum + 9].content = "1"
                             } else {
-                                fullGrid[cell.cellNum + 9] =
-                                        (fullGrid[cell.cellNum + 9].toInt() + 1).toString()
+                                fullGrid[cell.cellNum + 9].content =
+                                        (fullGrid[cell.cellNum + 9].content.toInt() + 1).toString()
                             }
                         }
                     }
 //                    checks if the surrounding cell exists using getOrNull
 //                    checks that when the column is size - 1 the cell modification doesn't take place because the bomb won't modify the cell two rows bellow
                     if (fullGrid.getOrNull(cell.cellNum + 10) != null && j != size - 1) {
-                        if (fullGrid[cell.cellNum + 10] != "X") {
-                            if (fullGrid[cell.cellNum + 10] == ".") {
-                                fullGrid[cell.cellNum + 10] = "1"
+                        if (!fullGrid[cell.cellNum + 10].isBomb) {
+                            if (fullGrid[cell.cellNum + 10].content == ".") {
+                                fullGrid[cell.cellNum + 10].content = "1"
                             } else {
-                                fullGrid[cell.cellNum + 10] =
-                                        (fullGrid[cell.cellNum + 10].toInt() + 1).toString()
+                                fullGrid[cell.cellNum + 10].content =
+                                        (fullGrid[cell.cellNum + 10].content.toInt() + 1).toString()
                             }
                         }
                     }
@@ -157,7 +175,7 @@ class Grid() {
     }
 
     //fun that for each row prints the grid's row
-    fun printGrid(fullGrid: Array<String>) {
+    fun printGrid(fullGrid: Array<Cell>) {
 //        prints the column numbers border
         print(" |")
         for (i in 1..size) print(i)
@@ -171,7 +189,7 @@ class Grid() {
 //            prints the row numbers
             print("${i + 1}|")
             for (j in 0 until size) {
-                print(fullGrid[i * size + j])
+                print(fullGrid[i * size + j].content)
             }
             println("|")
         }
@@ -179,6 +197,41 @@ class Grid() {
         print("-|")
         for (i in 1..size) print("-")
         println("|")
+    }
+
+    //    TODO doesnt recognize the second input if it has a space, only works when hitting enter
+    fun getMineSetting(row: InputStream, column: InputStream): Int {
+        println("Set/delete mines marks (x and y coordinates):")
+        return getCellNum(Scanner(row).next().toInt() - 1, Scanner(column).next().toInt() - 1)
+    }
+
+    fun changeGrid(grid: Grid, mineArray: IntArray) {
+        val playerMines = mutableListOf<Int>()
+//        var playerChoice = grid.getMineSetting(System.`in`, System.`in`)
+//        var needPrint = true
+        do {
+            val playerChoice = grid.getMineSetting(System.`in`, System.`in`)
+            when {
+                grid.fullGrid[playerChoice].content != "." && grid.fullGrid[playerChoice].content != "*" -> {
+                    println("There is a number here!")
+                    continue
+                }
+                grid.fullGrid[playerChoice].content == "." -> {
+                    grid.fullGrid[playerChoice].content = grid.fullGrid[playerChoice].content.replace(".", "*")
+                }
+                grid.fullGrid[playerChoice].content == "*" -> {
+                    grid.fullGrid[playerChoice].content = grid.fullGrid[playerChoice].content.replace("*", ".")
+                }
+            }
+            if (playerMines.contains(playerChoice)) {
+                playerMines.remove(playerChoice)
+            } else {
+                playerMines.add(playerChoice)
+            }
+            grid.printGrid(fullGrid)
+
+        } while (HashSet(mineArray.asList()) != HashSet(playerMines))
+        println("Congratulations! You found all the mines!")
     }
 }
 
@@ -189,5 +242,11 @@ fun main() {
     val grid = Grid()
     grid.makeGrid(mineArray)
     grid.printGrid(grid.fullGrid)
+    grid.changeGrid(grid, mineArray)
+//    while (HashSet(mineArray.asList()) != HashSet(playerMines)) {
+//        if (grid.changeGrid(grid)) grid.printGrid(grid.fullGrid)
+//        if (playerMines.contains())
+//    }
+
 }
 
